65c9e88de84a12aa4a35e7379f53ece5a6487a92
Art Karasev <as.karasyov@gmail.com>
2023-07-23 23:31:33 +1200

    block page

---
+++ src/Controllers/BlocksController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Goldfinch\Basement\Controllers;
+
+use PageController;
+
+class BlocksController extends PageController
+{
+    private static $allowed_actions = [];
+
+    private static $casting = [];
+
+    protected function init()
+    {
+        parent::init();
+
+        // ..
+    }
+}
