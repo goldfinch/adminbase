e8cf01a9c648f938db349d0c7bcaa2db94073ae4
Art Karasev <as.karasyov@gmail.com>
2023-07-31 21:25:27 +1200

    tweaks

--- src/Extensions/BaseElementExtension.php
+++ src/Extensions/BaseElementExtension.php
@@ -4,7 +4,6 @@ namespace Goldfinch\Basement\Extensions;
 
 use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
-use SilverStripe\Forms\LiteralField;
 
 class BaseElementExtension extends DataExtension
 {
@@ -22,11 +21,6 @@ class BaseElementExtension extends DataExtension
         $global = $fields->dataFieldByName('AvailableGlobally');
 
         $fields->insertAfter('ExtraClass', $global);
-    }
-
-    public function updateCMSActions(&$fields)
-    {
-        $fields->insertBefore('ActionMenus', LiteralField::create('test', '<a target="_blank" href="'.$this->owner->Link().'?stage=Stage" class="btn btn-primary bi bi-binoculars-fill me-1" title="Review page on the website"></a>'));
     }
 
     public function onBeforeWrite()
