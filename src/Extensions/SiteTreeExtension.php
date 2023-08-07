8e41f89d6196d35fd48dfe0de11983198fb6b702
Art Karasev <as.karasyov@gmail.com>
2023-08-07 22:52:40 +1200

    hotfix

--- src/Extensions/SiteTreeExtension.php
+++ src/Extensions/SiteTreeExtension.php
@@ -10,7 +10,10 @@ class SiteTreeExtension extends BaseSiteTreeExtension
 {
     public function updateCMSActions(FieldList $actions)
     {
-        $actions->insertBefore('ActionMenus', LiteralField::create('test', '<a target="_blank" href="'.$this->owner->Link().'?stage=Stage" class="btn btn-primary bi bi-binoculars-fill me-1" title="Review page on the website"></a>'));
+        if ($this->owner->hasMethod('Link'))
+        {
+            $actions->insertBefore('ActionMenus', LiteralField::create('test', '<a target="_blank" href="'.$this->owner->Link().'?stage=Stage" class="btn btn-primary bi bi-binoculars-fill me-1" title="Review page on the website"></a>'));
+        }
     }
 
     // public function onBeforeWrite()
