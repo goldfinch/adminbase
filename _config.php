4e091618adef27b955f95299f2080ffbfe1235d9
Art Karasev <as.karasyov@gmail.com>
2023-10-16 01:27:52 +1300

    add hotfix for mysql 5.7

--- _config.php
+++ _config.php
@@ -13,3 +13,6 @@ if (Director::isDev() && Environment::hasEnv('SS_DATABASE_SOCKET'))
 {
     ini_set('mysqli.default_socket', Environment::getEnv('SS_DATABASE_SOCKET'));
 }
+
+// fixing errors in mysql 5.7+
+SilverStripe\ORM\DB::query("SET SESSION sql_mode='REAL_AS_FLOAT,PIPES_AS_CONCAT,ANSI_QUOTES,IGNORE_SPACE';");
