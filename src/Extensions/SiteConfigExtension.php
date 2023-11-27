57d186110ac74c745e2be2cb9045d8cb6957d2af
Art Karasev <as.karasyov@gmail.com>
2023-11-27 13:27:43 +1300

    move social media to separate module

--- src/Extensions/SiteConfigExtension.php
+++ src/Extensions/SiteConfigExtension.php
@@ -7,8 +7,6 @@ use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\EmailField;
 use SilverStripe\ORM\DataExtension;
-use SilverStripe\Forms\CheckboxField;
-use SilverStripe\Forms\CompositeField;
 use SilverStripe\ORM\ValidationResult;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 
@@ -19,17 +17,6 @@ class SiteConfigExtension extends DataExtension
         'GeneralPhone' => 'Varchar',
         'GeneralAddress' => 'Varchar',
         'GeneralCopyright' => 'Varchar',
-
-        'GeneralFacebook' => 'Boolean',
-        'GeneralFacebookURL' => 'Varchar',
-        'GeneralInstagram' => 'Boolean',
-        'GeneralInstagramURL' => 'Varchar',
-        'GeneralTwitter' => 'Boolean',
-        'GeneralTwitterURL' => 'Varchar',
-        'GeneralLinkedIn' => 'Boolean',
-        'GeneralLinkedInURL' => 'Varchar',
-        'GeneralYouTube' => 'Boolean',
-        'GeneralYouTubeURL' => 'Varchar',
     ];
 
     private static $has_one = [
@@ -54,50 +41,10 @@ class SiteConfigExtension extends DataExtension
 
         $fields->addFieldsToTab('Root.Configurations', []);
 
-        $fields->addFieldsToTab('Root.SocialMedia', [
-
-            CompositeField::create(
-
-                CheckboxField::create('GeneralFacebook', 'Facebook'),
-                TextField::create('GeneralFacebookURL', '')->setAttribute('placeholder', 'https://facebook.com/...')->displayIf('GeneralFacebook')->isChecked()->end(),
-
-            )->setName('GeneralFacebookHolder'),
-
-            CompositeField::create(
-
-                CheckboxField::create('GeneralInstagram', 'Instagram'),
-                TextField::create('GeneralInstagramURL', '')->setAttribute('placeholder', 'https://www.instagram.com/...')->displayIf('GeneralInstagram')->isChecked()->end(),
-
-            )->setName('GeneralInstagramHolder'),
-
-            CompositeField::create(
-
-                CheckboxField::create('GeneralTwitter', 'Twitter'),
-                TextField::create('GeneralTwitterURL', '')->setAttribute('placeholder', 'https://twitter.com/...')->displayIf('GeneralTwitter')->isChecked()->end(),
-
-            )->setName('GeneralTwitterHolder'),
-
-            CompositeField::create(
-
-                CheckboxField::create('GeneralLinkedIn', 'LinkedIn'),
-                TextField::create('GeneralLinkedInURL', '')->setAttribute('placeholder', 'https://www.linkedin.com/...')->displayIf('GeneralLinkedIn')->isChecked()->end(),
-
-            )->setName('GeneralLinkedInHolder'),
-
-            CompositeField::create(
-
-                CheckboxField::create('GeneralYouTube', 'YouTube'),
-                TextField::create('GeneralYouTubeURL', '')->setAttribute('placeholder', 'https://www.youtube.com/...')->displayIf('GeneralYouTube')->isChecked()->end(),
-
-            )->setName('GeneralYouTubeHolder'),
-
-        ])->findTab('Root.SocialMedia')->setTitle('Social Media');
-
         // Tabs Reorder
         $fields = $this->orderTabs([
             'Configurations',
             'Forms',
-            'SocialMedia',
             'CookieConsent',
         ], $fields);
 
