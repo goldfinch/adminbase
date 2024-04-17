2b85e27bb8d3408420c310b353e94df4db4b5797
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-04-18 08:22:17 +1200

    WIP

--- _config.php
+++ _config.php
@@ -1,1 +1,8 @@
 <?php
+
+use SilverStripe\Admin\CMSMenu;
+use Symbiote\QueuedJobs\Controllers\QueuedJobsAdmin;
+
+if (class_exists(QueuedJobsAdmin::class)) {
+    CMSMenu::remove_menu_class(QueuedJobsAdmin::class);
+}
