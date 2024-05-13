b6eb720219ac9df5f354eec611c724b3b163c5d7
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-05-14 09:39:26 +1200

    WIP

--- _config.php
+++ _config.php
@@ -1,8 +1,11 @@
 <?php
 
+use Spatie\Ignition\Ignition;
 use SilverStripe\Admin\CMSMenu;
 use Symbiote\QueuedJobs\Controllers\QueuedJobsAdmin;
 
 if (class_exists(QueuedJobsAdmin::class)) {
     CMSMenu::remove_menu_class(QueuedJobsAdmin::class);
 }
+
+Ignition::make()->register();
